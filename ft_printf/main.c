e5e6c42f65fbd47eb253530847aee7e1d8933898
ctthanhthao <ctthanhthao@users.noreply.github.com>
2024-11-01 10:28:00 +0100

    update main

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -9,7 +9,7 @@ int main ()
 	int r = 0;
 	int r1;
 
-/*	// c
+	// c
 	printf("===================================\n");
 	r = ft_printf("ft char |%02c|\n", '0' + 256);
 	r1 = printf("pt char |%02c|\n", '0' + 256);
@@ -41,43 +41,9 @@ LONG_MIN, ULONG_MAX, 0, -42);
 	printf("ft_printf %i - printf %i\n", r, r1);
 	// i
 	printf("===================================\n");
-	r = ft_printf("|%5p|\n", (void *)0);
-	r1 = printf("|%5p|\n", (void *)0);
-	printf("ft_printf %i - printf %i\n", r, r1);
-*/	
-	// char *buffer;
-    // size_t size;
-    
-    // // Open a memory stream to capture output
-    // FILE *memstream = open_memstream(&buffer, &size);
-    // if (!memstream) {
-    //     perror("open_memstream failed");
-    //     return 1;
-    // }
-
-    // // Save original stdout and redirect to memory stream
-    // FILE *old_stdout = stdout;
-    // stdout = memstream;
-
- //   ft_printf("ft |%.d|, %.d, %.d, %.d, %.d, %.d, %.d, %.d", 0, 5, -1, -10, 100, -1862, INT_MIN, INT_MAX);
-	//printf("pt %.d, %.d, %.d, %.d, %.d, %.d, %.d, %.d", 0, 5, -1, -10, 100, -1862, INT_MIN, INT_MAX);
-    // Restore stdout
-    // fflush(stdout);
-    // stdout = old_stdout;
-    // fclose(memstream);
-
-    // printf("Captured output: %s", buffer);  // Print the captured output
-    //free(buffer);  // Free the memory allocated by open_memstream
-/*	r = ft_printf("%01.x, %01.x, %01.x, %01.x, %01.x, %01.x, %01.x, %01.x, %01.x, %01.x\n", 0, 5, -1, -10, 0x1234, -1862, 0xABCDE, INT_MIN, INT_MAX, 
-UINT_MAX);
-	r1 = printf("%01.x, %01.x, %01.x, %01.x, %01.x, %01.x, %01.x, %01.x, %01.x, %01.x\n", 0, 5, -1, -10, 0x1234, -1862, 0xABCDE, INT_MIN, INT_MAX, 
-UINT_MAX);
-	printf("ft_printf %i - printf %i\n", r, r1);
-	r = ft_printf("%5p, %5p, %5p, %5p, %5p, %5p\n", (void *)0, (void *)0xABCDE,
-	 (void *)ULONG_MAX, (void *)LONG_MIN, (void *)-1, (void *)-2352);
-	r1 = printf("%5p, %5p, %5p, %5p, %5p, %5p\n", (void *)0, (void *)0xABCDE,
-	  (void *)ULONG_MAX, (void *)LONG_MIN, (void *)-1, (void *)-2352);
-	printf("ft_printf %i - printf %i\n", r, r1);
+	r = ft_printf("ft i %u %i %i %i %i\n", INT_MAX, INT_MIN, LONG_MAX, 0, -42);
+	r1 = printf("pt i %i %i %i %i %i\n", INT_MAX, INT_MIN, LONG_MAX, 0, -42);
+	printf("ft_printf %i - printf %i\n", r, r1);	
 	// u
 	printf("===================================\n");
 	r = ft_printf("ft u %u \n", 0);
@@ -87,15 +53,18 @@ UINT_MAX);
 	r = ft_printf("ft u %u %u %u %u %u\n", INT_MAX, INT_MIN, LONG_MAX, 0, -42);
 	r1 = printf("pt u %u %u %u %u %u\n", INT_MAX, INT_MIN, LONG_MAX, 0, -42);
 	printf("ft_printf %i - printf %i\n", r, r1);	
-*//*	// p
+	// p
 	printf("===================================\n");
-	int value = 12345;
-	int *pt = &value;
 	r = ft_printf("ft pointer |%-13p| |%-14p| \n", ULONG_MAX, -ULONG_MAX);
 	r1 = printf("pt pointer |%-13p| |%-14p| \n", ULONG_MAX, -ULONG_MAX);
 	printf("ft_printf %i - printf %i\n", r, r1);
 	r = ft_printf("ft pointer |%-153p%0110.8x%110p|\n" ,(void*)17240180584784891087lu,2705171059u,(void*)1312307382483808423lu);
 	r1 = printf("pt pointer |%-153p%0110.8x%110p|\n" ,(void*)17240180584784891087lu,2705171059u,(void*)1312307382483808423lu);
+	printf("ft_printf %i - printf %i\n", r, r1);
+	r = ft_printf("ft pointer %5p, %5p, %5p, %5p, %5p, %5p\n", (void *)0, (void *)0xABCDE,
+	 (void *)ULONG_MAX, (void *)LONG_MIN, (void *)-1, (void *)-2352);
+	r1 = printf("pt pointer %5p, %5p, %5p, %5p, %5p, %5p\n", (void *)0, (void *)0xABCDE,
+	  (void *)ULONG_MAX, (void *)LONG_MIN, (void *)-1, (void *)-2352);
 	printf("ft_printf %i - printf %i\n", r, r1);
 	r = ft_printf("ft NULL pointer %p\n", NULL);
 	r1 = printf("pt NULL pointer %p\n", NULL);
@@ -108,10 +77,15 @@ UINT_MAX);
 	r = ft_printf("ft hex %#x %#x %#x %#x %#x %#x %#x\n", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, ULONG_MAX, 0, -42);
 	r1 = printf("pt hex %#x %#x %#x %#x %#x %#x %#x\n", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, ULONG_MAX, 0, -42);
 	printf("ft_printf %i - printf %i\n", r, r1);
-*/	r = ft_printf("%#5.x\n", 5);
-	r1 = printf("%#5.x\n", 5);
+	r = ft_printf("ft hex %01.x, %01.x, %01.x, %01.x, %01.x, %01.x, %01.x, %01.x, %01.x, %01.x\n", 0, 5, -1, -10, 0x1234, -1862, 0xABCDE, INT_MIN, INT_MAX, 
+UINT_MAX);
+	r1 = printf("pt hex %01.x, %01.x, %01.x, %01.x, %01.x, %01.x, %01.x, %01.x, %01.x, %01.x\n", 0, 5, -1, -10, 0x1234, -1862, 0xABCDE, INT_MIN, INT_MAX, 
+UINT_MAX);
 	printf("ft_printf %i - printf %i\n", r, r1);
-/*	// X
+	r = ft_printf("ft hex %#5.x\n", 5);
+	r1 = printf("pt hex %#5.x\n", 5);
+	printf("ft_printf %i - printf %i\n", r, r1);
+	// X
 	printf("===================================\n");
 	r = ft_printf("ft HEX %#X %#X\n", 16, -100);
 	r1 = printf("pt HEX %#X %#X\n", 16, -100);
@@ -127,25 +101,20 @@ UINT_MAX);
 	// NULL
 	printf("===================================\n");
 	r = ft_printf(NULL);
+	printf("\n");
 //	r1 = printf(NULL);
+//	printf("\n");
 	printf("NULL ft_printf %i - printf %i\n", r, r1);
+//	r = ft_printf(0);
+//	r1 = printf(0);
+//	printf("test with 0 ft_printf %i - printf %i\n", r, r1);
 	// test random
 	printf("================= random test ==================\n");
 	r = ft_printf("ft \001\002\007\v\010\f\r\n");
 	r1 = printf("pt \001\002\007\v\010\f\r\n");
 	printf("ft_printf %i - printf %i\n", r, r1);
-	
-/*	int n = 0;
-	int re = -(n % 10);
-	int *p = &n;
-	const char buff[16];
-	printf("p is |%#%%d|\n", 2551);
-//	printf("len is %i\n", ft_strlen(buff));
-//	printf("|%010.4li|\n", 25566666666666);
-//	printf("|%.5f|\n", 56.0);
-//	printf("|%---.8s|\n", "Hi");
-//	printf("% 5#d\n", 255);
-//	printf("|%5d|\n", -42);
-*/
+	printf("================= test unexpected conversion ==================\n");
+	r = ft_printf("ft %a %%%% %i %#f %u\n", 0, 7.80, INT_MIN, INT_MIN);
+	r1 = printf("pt %u\n", INT_MIN);
 	return (0);
 }
