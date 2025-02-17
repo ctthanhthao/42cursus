49df5191751e639d6a015bc648c9a0a955213121
ctthanhthao <49538317+ctthanhthao@users.noreply.github.com>
2025-02-17 14:03:03 +0100

    push_swap: fix issue : input instruction name which covers original one
    for ex: input saa then sa will be executed.
    Expect: return error

--- push_swap/bonus/checker_bonus.c
+++ push_swap/bonus/checker_bonus.c
@@ -11,36 +11,36 @@
 /* ************************************************************************** */
 
 #include "../include/push_swap_bonus.h"
-#include <stdio.h>
 
 static int	execute(t_stack **st_a, t_stack **st_b, char *ins)
 {
-	if (ft_strncmp(ins, "sa", 2) == 0)
+	if (ft_strncmp(ins, "ss\n", 3) == 0)
+		swap_top_both_stacks(st_a, st_b);
+	else if (ft_strncmp(ins, "sa\n", 3) == 0)
 		swap_top_no_print(st_a);
-	else if (ft_strncmp(ins, "sb", 2) == 0)
+	else if (ft_strncmp(ins, "sb\n", 3) == 0)
 		swap_top_no_print(st_b);
-	else if (ft_strncmp(ins, "pa", 2) == 0)
+	else if (ft_strncmp(ins, "pa\n", 3) == 0)
 		push_s2_to_s1(st_a, st_b);
-	else if (ft_strncmp(ins, "pb", 2) == 0)
+	else if (ft_strncmp(ins, "pb\n", 3) == 0)
 		push_s2_to_s1(st_b, st_a);
-	else if (ft_strncmp(ins, "ra", 2) == 0)
+	else if (ft_strncmp(ins, "ra\n", 3) == 0)
 		rotate_no_print(st_a);
-	else if (ft_strncmp(ins, "rb", 2) == 0)
+	else if (ft_strncmp(ins, "rb\n", 3) == 0)
 		rotate_no_print(st_b);
-	else if (ft_strncmp(ins, "rrr", 3) == 0)
+	else if (ft_strncmp(ins, "rrr\n", 4) == 0)
 		both_reverse_rotate_no_print(st_a, st_b);
-	else if (ft_strncmp(ins, "rra", 3) == 0)
+	else if (ft_strncmp(ins, "rra\n", 4) == 0)
 		reverse_rotate_no_print(st_a);
-	else if (ft_strncmp(ins, "rrb", 3) == 0)
+	else if (ft_strncmp(ins, "rrb\n", 4) == 0)
 		reverse_rotate_no_print(st_b);
-	else if (ft_strncmp(ins, "rr", 2) == 0)
+	else if (ft_strncmp(ins, "rr\n", 3) == 0)
 		both_rotate_no_print(st_a, st_b);
 	else
 		return (ft_stack_free(st_a), ft_stack_free(st_b), 0);
 	return (1);
 }
 
-// Function to read and execute instructions on the stacks
 void	execute_ins(t_stack **st_a, t_stack **st_b)
 {
 	char	*ins;
@@ -73,9 +73,6 @@ int	main(int argc, char **argv)
 		ft_stack_free(&st_a);
 		ft_error();
 	}
-	if (ft_stack_size(st_a) == 1)
-		return (write_intr("OK", ' '),
-			ft_stack_free(&st_a), ft_stack_free(&st_b), 0);
 	execute_ins(&st_a, &st_b);
 	if (is_asc_sorted(st_a) && ft_stack_size(st_b) == 0)
 		write_intr("OK", ' ');
@@ -83,29 +80,3 @@ int	main(int argc, char **argv)
 		write_intr("KO", ' ');
 	return (ft_stack_free(&st_a), ft_stack_free(&st_b), 0);
 }
-/*
-int	main()
-{
-	int argc = 2;
- 	char *argv[] = {"test","2 1"};
-	t_stack	*st_a;
-	t_stack	*st_b;
-
-	if (argc < 2)
-		return (0);
-	st_a = ft_process(argc, argv);
-	st_b = NULL;
-	if (!st_a || ft_checkdup(st_a))
-	{
-		ft_stack_free(&st_a);
-		ft_error();
-	}
-	if (ft_stack_size(st_a) == 1)
-		return (ft_stack_free(&st_a), ft_stack_free(&st_b), 0);
-	execute_ins(&st_a, &st_b);
-	if (is_asc_sorted(st_a) && ft_stack_size(st_b) == 0)
-		write_intr("OK", ' ');
-	else
-		write_intr("KO", ' ');
-	return (ft_stack_free(&st_a), ft_stack_free(&st_b), 0);
-}*/
