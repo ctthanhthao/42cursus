b54ca6c9e06e126e8c87869a5a9fa685ba9e91ec
ctthanhthao <ctthanhthao@users.noreply.github.com>
2025-11-06 20:00:05 +0100

    update main

--- CPP-Module-03/ex01/main.cpp
+++ CPP-Module-03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: thchau <thchau@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 12:16:52 by thchau            #+#    #+#             */
-/*   Updated: 2025/10/29 12:45:39 by thchau           ###   ########.fr       */
+/*   Updated: 2025/11/06 19:59:42 by thchau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,18 @@ int main(void)
 	ScavTrap Foo("Foo");
 	Foo.attack("Bar");
 	Foo.attack("Cho");
+	Foo.takeDamage(80);
 
 	// copy Foo
-	ScavTrap FooChild = Foo;
+	ScavTrap FooChild("FooChild");
 	FooChild.attack("BarChild");
 	FooChild.guardGate();
+	
+	FooChild = Foo;
+	FooChild.takeDamage(10);
+	std::cout << "Finish assigned and start copying." << std::endl;
+	ScavTrap assign(Foo);
+	assign.attack("AssignTest");
+	assign.takeDamage(50);
 	return 0;
 }
