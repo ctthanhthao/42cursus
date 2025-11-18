5880d0b401faf1b27bb5c5870c897169e9bf6708
ctthanhthao <ctthanhthao@users.noreply.github.com>
2025-11-18 09:51:24 +0100

    add cat test

--- CPP-Module-04/ex01/main.cpp
+++ CPP-Module-04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: thchau <thchau@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 15:03:46 by thchau            #+#    #+#             */
-/*   Updated: 2025/11/18 09:10:45 by thchau           ###   ########.fr       */
+/*   Updated: 2025/11/18 09:50:04 by thchau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int main()
 	for (int i = 0; i < 10; i++)
 		delete animals[i];
 	
-	std::cout << "\n=== Animal with brain test ===\n" << std::endl;
+	std::cout << "\n=== Dog with brain test ===\n" << std::endl;
 	Dog *dog = new Dog();
 	dog->getBrain()->setIdea(0, "Go to the zoo.");
 	
@@ -53,11 +53,40 @@ int main()
 			  << std::endl;
 	std::cout << "Dog_copy's idea: " << dog_copy->getBrain()->getIdea(0)
 			  << std::endl;
+	std::cout << "\n=== Cat with brain test ===\n" << std::endl;
+	Cat *cat = new Cat();
+	cat->getBrain()->setIdea(0, "I want a fish.");
+	
+	Cat *cat_assign = new Cat();
+	*cat_assign = *cat;
+
+	Cat *cat_copy = new Cat(*cat);
+	
+	std::cout << "Cat's idea: " << cat->getBrain()->getIdea(0)
+			  << std::endl;
+	std::cout << "Cat_assign's idea: " << cat_assign->getBrain()->getIdea(0)
+			  << std::endl;
+	std::cout << "Cat_copy's idea: " << cat_copy->getBrain()->getIdea(0)
+			  << std::endl;
+			  
+	std::cout << "\n-- Changing cat idea --\n" << std::endl;
+	cat->getBrain()->setIdea(0, "No, i will eat a banana.");
+	std::cout << "Cat's idea: " << cat->getBrain()->getIdea(0)
+			  << std::endl;
+	cat_assign->getBrain()->setIdea(0, "No, i will eat a grape.");
+	std::cout << "cat_assign's idea: " << cat_assign->getBrain()->getIdea(0)
+			  << std::endl;
+	std::cout << "cat_copy's idea: " << cat_copy->getBrain()->getIdea(0)
+			  << std::endl;
 	std::cout << "\n=== Destructor ====\n" << std::endl;
 
 	delete dog;
 	delete dog_copy;
 	delete dog_assign;
+
+	delete cat;
+	delete cat_copy;
+	delete cat_assign;
 	
 	return 0;
 }
